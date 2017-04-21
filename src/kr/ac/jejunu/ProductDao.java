3aa5002e8cfce1fb14207afc6a2cc5a0760b7d3b
WoochanKim <woochankim200@gmail.com>
2017-04-21 18:02:17 +0900

    delete useless class and replace that with callback.

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -11,24 +11,50 @@ public class ProductDao {
     }
 
     public Product get(Long id) throws ClassNotFoundException, SQLException {
-        PrepareStatementStrategy prepareStatementStrategy = new GetPrepareStatementStrategy(id);
+        PrepareStatementStrategy prepareStatementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
         return jdbcContext.selectQuery(prepareStatementStrategy);
     }
 
     public void add(Product product) throws ClassNotFoundException, SQLException {
-        PrepareStatementStrategy prepareStatementStrategy = new AddPrepareStatementStrategy(product);
+        PrepareStatementStrategy prepareStatementStrategy = connection -> {
+            long id = product.getId();
+            String title = product.getTitle();
+            int price = product.getPrice();
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into product(id, title, price) values (?, ?, ?)");
+            preparedStatement.setLong(1, id);
+            preparedStatement.setString(2, title);
+            preparedStatement.setInt(3, price);
+            return preparedStatement;
+        };
         jdbcContext.updateQuery(prepareStatementStrategy);
     }
 
 
     public void update(Product product) throws SQLException {
-        PrepareStatementStrategy prepareStatementStrategy = new UpdatePrepareStatementStrategy(product);
+        PrepareStatementStrategy prepareStatementStrategy = connection -> {
+            long id = product.getId();
+            String title = product.getTitle();
+            int price = product.getPrice();
+            PreparedStatement preparedStatement = connection.prepareStatement("update product set title=?, price=? where id =?");
+            preparedStatement.setString(1, title);
+            preparedStatement.setInt(2, price);
+            preparedStatement.setLong(3, id);
+            return preparedStatement;
+        };
         jdbcContext.updateQuery(prepareStatementStrategy);
 
     }
 
     public void delete(Long id) throws SQLException {
-        PrepareStatementStrategy prepareStatementStrategy = new DeletePrepareStatementStrategy(id);
+        PrepareStatementStrategy prepareStatementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from product where id = ?");
+            preparedStatement.setLong(1,id);
+            return preparedStatement;
+        };
         jdbcContext.updateQuery(prepareStatementStrategy);
     }
 }
