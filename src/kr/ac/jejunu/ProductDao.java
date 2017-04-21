c3bf62ca775edfb1ed0d34073316e6dc9da8f3d5
WoochanKim <woochankim200@gmail.com>
2017-04-21 18:23:34 +0900

    replace jdbcContext to jdbcTemplate.

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -1,40 +1,55 @@
 package kr.ac.jejunu;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+
 import java.sql.*;
 
 public class ProductDao {
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public Product get(Long id) throws ClassNotFoundException, SQLException {
         String sql = "select * from product where id = ?";
         Object[] params = new Object[]{id};
-        PrepareStatementStrategy prepareStatementStrategy = jdbcContext.makePrepareStatement(sql, params);
-        return jdbcContext.selectQuery(prepareStatementStrategy);
+        Product result = null;
+        try {
+            result = jdbcTemplate.query(sql, params, resultSet -> {
+                Product product = null;
+                if(resultSet.next()) {
+                    product = new Product();
+                    product.setId(resultSet.getLong("id"));
+                    product.setPrice(resultSet.getInt("price"));
+                    product.setTitle(resultSet.getString("title"));
+                }
+                return product;
+            });
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public void add(Product product) throws ClassNotFoundException, SQLException {
         String sql = "insert into product(id, title, price) values (?, ?, ?)";
         Object[] params = new Object[]{product.getId(), product.getTitle(), product.getPrice()};
-        PrepareStatementStrategy prepareStatementStrategy = jdbcContext.makePrepareStatement(sql, params);
-        jdbcContext.updateQuery(prepareStatementStrategy);
+        jdbcTemplate.update(sql, params);
     }
 
     public void update(Product product) throws SQLException {
         String sql = "update product set title=?, price=? where id =?";
         Object[] params = new Object[]{product.getTitle(), product.getPrice(), product.getId()};
-        PrepareStatementStrategy prepareStatementStrategy = jdbcContext.makePrepareStatement(sql, params);
-        jdbcContext.updateQuery(prepareStatementStrategy);
+        jdbcTemplate.update(sql, params);
 
     }
 
     public void delete(Long id) throws SQLException {
         String sql = "delete from product where id = ?";
         Object[] params = new Object[]{id};
-        PrepareStatementStrategy prepareStatementStrategy = jdbcContext.makePrepareStatement(sql, params);
-        jdbcContext.updateQuery(prepareStatementStrategy);
+        jdbcTemplate.update(sql, params);
     }
 }
